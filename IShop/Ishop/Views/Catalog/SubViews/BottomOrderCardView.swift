a004a50f4c21425272d2c3f46eda1952ea76327b
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-02 00:21:43 +0200

    Implement Order Viewing

---
+++ IShop/Ishop/Views/Catalog/SubViews/BottomOrderCardView.swift
@@ -0,0 +1,55 @@
+//
+//  BottomOrderCardView.swift
+//  Ishop
+//
+//  Created by Cédric Bahirwe on 02/05/2021.
+//
+
+import SwiftUI
+
+struct BottomOrderCardView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Rectangle().frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            
+            Text("Current order")
+                .multilineTextAlignment(.center)
+                .font(Font.headline.bold())
+                .lineSpacing(4)
+                .foregroundColor(.black)
+            Divider()
+            VStack {
+                
+                HStack {
+                    Image(systemName: "cart.fill")
+                    Text("3 products")
+                    Spacer()
+                    
+                    Text("$45.5")
+                }
+                .font(Font.callout.weight(.semibold))
+                .padding()
+                .foregroundColor(.mainBackground)
+                .background(Color.mainDark)
+                .cornerRadius(8)
+            }
+            .padding(.horizontal, 10)
+
+            Spacer( )
+        }
+        .padding(.top, 8)
+        .frame(maxWidth: .infinity)
+        .background(Color.mainBackground)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+
+struct BottomOrderCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BottomOrderCardView()
+    }
+}
