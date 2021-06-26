8a04caed330d2bdb794f36121d230873e48005d3
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-26 14:33:31 +0200

    Add Spinning button

--- IShop/Ishop/Views/Carts/AccountDetails.swift
+++ IShop/Ishop/Views/Carts/AccountDetails.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AccountDetails: View {
     @State private var showPassword: Bool = false
     @State private var password: String = ""
+    @State private var State: LoadingButton.ButtonState = .initial
     var body: some View {
         VStack(alignment: .leading) {
             HStack {
@@ -65,7 +66,12 @@ struct AccountDetails: View {
             .padding(10)
             Divider()
             
-            Button(action: {}, label: {
+            Button(action: {
+                withAnimation {
+                    State = State == .initial ? .loading : State == .loading ? .finished : .initial
+                    
+                }
+            }, label: {
                 Text("Continue")
                     .font(Font.callout.weight(.semibold))
                     .padding()
@@ -75,6 +81,9 @@ struct AccountDetails: View {
                     .cornerRadius(8)
             })
             .padding(10)
+            LoadingButton(title: "Continue", state: State)
+                .padding()
+            
         }
     }
 }
@@ -82,5 +91,56 @@ struct AccountDetails: View {
 struct AccountDetails_Previews: PreviewProvider {
     static var previews: some View {
         AccountDetails()
+    }
+}
+
+
+struct LoadingButton: View {
+    
+    enum ButtonState {
+        case initial, loading, finished
+    }
+    let title: String
+    
+    var state: ButtonState
+    @State private var animate: Bool = false
+    
+    let action: () -> ()
+    var body: some View {
+        Button(action: action) {
+            ZStack {
+                switch state {
+                case .initial:
+                    Text(title)
+                        .font(Font.callout.weight(.semibold))
+                    
+                case .loading:
+                    
+                    Circle()
+                        .trim(from: 0, to: 0.7)
+                        .stroke(Color.white, lineWidth: 2)
+                        .frame(width: 20, height: 20)
+                        .rotationEffect(Angle(degrees: animate ? 360 : 0))
+                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    Image(systemName: "circle.dashed")
+                        .imageScale(.large)
+                        .rotationEffect(Angle(degrees: animate ? 360 : 0))
+                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                default :
+                    Image(systemName: "checkmark.circle")
+                        .imageScale(.large)
+                }
+            }
+            .padding()
+            .foregroundColor(.mainBackground)
+            .frame(maxWidth: .infinity)
+            .frame(height: 50)
+            .background(Color.mainDark)
+            .cornerRadius(8)
+            .animation(.easeIn)
+            .onChange(of: state) {
+                animate = $0 == .loading ? true : false
+            }
+        }
     }
 }
