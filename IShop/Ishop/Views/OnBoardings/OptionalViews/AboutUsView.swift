27fa964d5bf19f7d6db9a9011a3f679ebc938eda
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-26 05:30:36 +0200

    Build About Us

--- IShop/Ishop/Views/OnBoardings/OptionalViews/AboutUsView.swift
+++ IShop/Ishop/Views/OnBoardings/OptionalViews/AboutUsView.swift
@@ -11,11 +11,28 @@ struct AboutUsView: View {
     var body: some View {
         VStack {
             TopNavigationHeaderView(title: "About IShop")
-            RoundedRectangle()
-//                .fr
-//            TopNavigationHeaderView(didPop: <#T##(() -> ())?##(() -> ())?##() -> ()#>)
-            Text("Hello, World!")
-//            Spacer()
+            VStack(alignment: .leading, spacing: 16) {
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(.secondarySystemBackground))
+                        .frame(height: 100)
+                    Text("IShop™ 2021")
+                        .bold()
+                        .padding(8)
+                    
+                }
+                
+                Label("About us", systemImage: "arrow.up.forward.app")
+                Label("Terms and Conditions", systemImage: "arrow.up.forward.app")
+                Label("Privacy Policy", systemImage: "arrow.up.forward.app")
+                Label("Carrers", systemImage: "arrow.up.forward.app")
+                Label("Speak with Zarba", systemImage: "arrow.up.forward.app")
+
+                
+            }
+            .padding(12)
+            
+            Spacer()
         }
     }
 }
