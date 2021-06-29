25d2a24ad7bec855795e9b724e21662b3a10373d
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-29 15:22:58 +0200

    wrap friend referal view

--- IShop/Ishop/Views/Catalog/SubViews/SingleProductView.swift
+++ IShop/Ishop/Views/Catalog/SubViews/SingleProductView.swift
@@ -7,7 +7,46 @@
 
 import SwiftUI
 
+struct ProductOrder: Identifiable {
+    let id = UUID()
+    let name: String
+    let description: String?
+    let quantity: Int
+    private(set) var orderQuantity: Int = 0
+    let price: Double
+    var tags: [String]
+    
+    var remainingQuantity: Int {
+        quantity - orderQuantity
+    }
+    var totalPrice: Double {
+        Double(orderQuantity) * price
+    }
+    
+    mutating func increaseQty() {
+        if orderQuantity < quantity {
+            orderQuantity += 1
+        }
+    }
+    
+    mutating func decreaseQty() {
+        if orderQuantity > 0 {
+            orderQuantity -= 1
+        }
+    }
+}
+
 struct SingleProductView: View {
+    @State private var productOrder = ProductOrder(
+        name: "Ice Breakers",
+        description: "Ice cube, wintergreen, 40ct",
+        quantity: 16,
+        price: 1.40,
+        tags: ["Low Stock", "Great Value"])
+    
+    private var isQtyEmpty: Bool {
+        productOrder.orderQuantity == 0
+    }
     var body: some View {
         VStack(alignment: .leading) {
             Color.lightGray
@@ -15,14 +54,16 @@ struct SingleProductView: View {
                 .frame(maxWidth: .infinity)
                 .padding(.bottom, 20)
             
-            Text("Ice Breakers")
+            Text(productOrder.name)
                 .font(Font.title.bold())
-            Text("Ice cube, wintergreen, 40ct,")
-            Text("QTY: 12")
+            if let description = productOrder.description {
+                Text(description)
+            }
+            Text("QTY: \(productOrder.remainingQuantity)")
                 .foregroundColor(.secondary)
             
             HStack {
-                ForEach(["Low Stock", "Great Value"], id:\.self) { item in
+                ForEach(productOrder.tags, id:\.self) { item in
                     Text(item)
                         .font(.caption)
                         .padding(5)
@@ -34,57 +75,61 @@ struct SingleProductView: View {
                 }
             }
             
-            CheckoutBannerView(icon: "cart.fill",
-                               title: "Add to cart",
-                               price: "$1.4")
             
-            HStack(spacing: 0) {
+ 
+                HStack(spacing: 0) {
                 GeometryReader { geo in
                     let halfWidth = geo.size.width/2
                     HStack(spacing: 0) {
-                        Button(action: {}, label: {
-                            Image(systemName: "minus")
-                                .font(Font.caption.bold())
-                                .frame(width: 55, height: 55)
-                                .foregroundColor(.primary)
-                                .background(Color(.secondarySystemBackground))
-                                .cornerRadius(8)
-                        })
-                        
-                        .frame(width: halfWidth/3,
-                               alignment: .leading)
-                        
-                        Text("1")
-                            .font(Font.title3.bold())
-                            .frame(minWidth: 40, maxWidth: .infinity)
-                            .frame(width: halfWidth/3)
-                        
-                        Button(action: {}, label: {
-                            Image(systemName: "plus")
-                                .font(Font.caption.bold())
-                                .frame(width: 55, height: 55)
-                                .foregroundColor(.primary)
-                                .background(Color(.secondarySystemBackground))
-                                .cornerRadius(8)
-                        })
-                        .frame(width: halfWidth/3,
-                               alignment: .trailing)
-                        
-                        Button(action: {}) {
-                            CheckoutBannerView(icon: "cart.fill",
-                                               title: "View cart",
-                                               price: "")
-                                
+                        if !isQtyEmpty {
+                            Button(action: {
+                                withAnimation {
+                                    productOrder.decreaseQty()
+                                }
+                            }, label: {
+                                Image(systemName: "minus")
+                                    .font(Font.caption.bold())
+                                    .frame(width: 55, height: 55)
+                                    .foregroundColor(.primary)
+                                    .background(Color(.secondarySystemBackground))
+                                    .cornerRadius(8)
+                            })
+                            
+                            .frame(width: halfWidth/3,
+                                   alignment: .leading)
+                            
+                            Text(productOrder.orderQuantity.description)
+                                .font(Font.title3.bold())
+                                .frame(minWidth: 40, maxWidth: .infinity)
+                                .frame(width: halfWidth/3)
+                            
+                            Button(action: {
+                                productOrder.increaseQty()
+                            }, label: {
+                                Image(systemName: "plus")
+                                    .font(Font.caption.bold())
+                                    .frame(width: 55, height: 55)
+                                    .foregroundColor(.primary)
+                                    .background(Color(.secondarySystemBackground))
+                                    .cornerRadius(8)
+                            })
+                            .frame(width: halfWidth/3,
+                                   alignment: .trailing)
                         }
-                        .padding(.leading, 10)
-                        
+                        Button(action: {
+                            withAnimation {
+                                productOrder.increaseQty()
+                            }
+                        }) {
+                            CheckoutBannerView(
+                                icon: "cart.fill",
+                                title: isQtyEmpty ? "Add to cart" : "View cart",
+                                price: isQtyEmpty ? "$\(productOrder.price)" : "")
+                        }
+                        .padding(.leading,isQtyEmpty ? 0 : 10)
                     }
-                    
-                    
                 }
                 .frame(height: 55)
-                
-                
             }
         }
         .padding(10)
